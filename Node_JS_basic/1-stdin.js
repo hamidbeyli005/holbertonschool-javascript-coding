19c11857b0519d9e8959cabf914f487023ce7a39
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-05-26 15:33:33 +0400

    Update

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,15 +1,16 @@
-console.log('Welcome to Holberton School, what is your name?');
-// await input
-process.stdin.resume();
-// once input is entered
-process.stdin.on('readable', () => {
-  const name = process.stdin.read();
-  process.stdout.write(`Your name is: ${name}`);
-  // check if input is coming from the terminal
-  if (process.stdin.isTTY) {
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
-  } else {
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
     process.stdout.write('This important software is now closing\n');
-    process.exit();
-  }
-});
+  });
+}
